65c7bb254c6cbeca62fe7ff80e261dcce89786ed
IvanGrishyn <IvanGrishyn@gmail.com>
2021-05-18 03:03:54 +0300

    изменено:      bin/Notebook.dart добавлена генерация событий от клавиатуры

--- bin/Notebook.dart
+++ bin/Notebook.dart
@@ -1,4 +1,9 @@
 import 'package:event/event.dart';
+import "package:console/console.dart";
+import 'dart:io';
+import 'dart:convert';
+
+
 
 class ChangeArgs extends EventArgs {
   int ev_type;
@@ -13,20 +18,34 @@ void HandleEvent(ChangeArgs val){print("${val.key_data} ${val.msg}");}}
 
 
 class Application {
-void HandleEvent(){
-print ("event handler");
+void HandleEvent(ChangeArgs val){
+
+print (String.fromCharCodes(val.key_data));
+}
+
+void Run(){
+ChangeArgs ar;
+
 }}
 
 
 void main(){
-var  args = ChangeArgs(0,[1,2,3,4,5]," Привет");
-var e =Event<ChangeArgs>();
 
 var app=Application();
 var app2=Application2();
-e+(args)=>app.HandleEvent();
+
+
+var e =Event<ChangeArgs>();
+
+var  args = ChangeArgs(0,[1,2,3,4,5]," Привет");
+
+
+e+(args)=>app.HandleEvent(args);
 e+(args)=>app2.HandleEvent(args);
 
-e.broadcast(args);
+
+  stdin.echoMode = false;
+  stdin.lineMode = false;
+ stdin.listen((List<int>d){args.key_data=d; e.broadcast(args);}); //main cycle
 
 }
